8f152f9f7323d1ad002556a6e140b3ca3e66f021
hadleyrose <hadleyrosemitchell@gmail.com>
2021-09-08 16:02:06 -0500

    WIP: Updated bronte_quotes script
    * Added code to get all quotes from all Brontes organized in dict
      - key=Bronte, value=list of quotes
    * Added code to find a specific string-containing quote across all Brontes
      - Extended code to include author and quote
    
    NEXT STEPS:
    * Parse quotes for any related to greetings and farewells
    * Map Brontes and quotes to actions/intents
    * Test if source of quote is able to be determined; if not, incorporate
    * Build prototype bronte bot using mappings and data
      - MVP: greeting, farewell, random Branwell interjection, get quote from each sister

--- bronte_quotes.py
+++ bronte_quotes.py
@@ -14,3 +14,15 @@ wikiquote.quotes(emily, max_quotes=1)
 
 # Get random Branwell quote
 random.choice(wikiquote.quotes(branwell))
+
+# Get all quotes per Bronte
+results = {bronte: wikiquote.quotes(bronte, max_quotes=100) for bronte in brontes}
+
+# Find a quote containing a word across all Brontes
+[quote for result in results.values() for quote in result if 'morning' in quote.lower()]
+
+# Find a quote (and its author) containing a word across all Brontes
+print(*[f'{bronte}: {quote}' for bronte, result in results.items() for quote in result if 'doom' in quote.lower()], sep='\n')
+
+# Get source of quote found in results
+print(*[bronte for bronte, result in results.items() for quote in result if 'and he said he could not breathe in mine' in quote.lower()])
