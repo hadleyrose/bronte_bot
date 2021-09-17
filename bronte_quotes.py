139d23822b1404227412eb3f51dcb5ea3a94acc8
hadleyrose <hadleyrosemitchell@gmail.com>
2021-09-17 00:32:53 -0500

    WIP: Updated bronte_quotes script with hyponym_quote_finder function
    * Wrapped hyponym lemma retrieval and quote matching--both fuzzy and exact--in hyponym_quote_finder function
    * Added capability to limit to single source or specific Bronte sources
    * Added capability to return fuzzy matches if less than specified threshold matches returned from exact matches
    * Added capability to return fuzzy matches instead of exact matches
    
    NEXT STEPS:
    * Map Brontes and quotes to actions/intents
    * Test if source (book or letter, which book) of quote is able to be determined; if not, incorporate
    * Build prototype bronte bot using mappings and data
      - MVP: greeting, farewell, random Branwell interjection, get quote from each sister
    * Add Bronte icon for bot
      - Change icon depending on which quote is chosen?

--- bronte_quotes.py
+++ bronte_quotes.py
@@ -59,3 +59,35 @@ tests = [str(i).replace('_', ' ').replace('-', ' ') for syn in wn.synset('farewe
 print(*[f'{bronte, test}: {quote}' for bronte, result in results.items() for quote in result for test in tests if test in quote.lower()], sep='\n')
 # exact word match -- returns 25 results
 print(*[f'{bronte, test}: {quote}' for bronte, result in results.items() for quote in result for w in quote.split() for test in tests if test == w.lower()], sep='\n')
+
+def hyponym_quote_finder(
+    token_wordnet_tag: str,
+    source: str or list = None,
+    fuzzy: bool = False,
+    fuzzy_if: bool = True,
+    threshold = 10
+) -> list:
+    # full result set
+    quotes = results
+
+    # narrow result set if specified
+    if source:
+        if isinstance(source, str):
+            quotes = {bronte: quote for bronte, quote in quotes.items() if source == bronte}
+        
+        if isinstance(source, list):
+            quotes = {bronte: quote for bronte, quote in quotes.items() if bronte in source}
+
+    # get token_wordnet_tag lemma
+    tests = [str(i).replace('_', ' ').replace('-', ' ') for syn in wn.synset(token_wordnet_tag).hyponyms() for i in syn.lemma_names()]
+
+    # Get quotes containing lemma
+
+    # exact word match
+    matches = [(bronte, test, quote) for bronte, result in quotes.items() for quote in result for w in quote.split() for test in tests if test == w.lower()]
+
+    if fuzzy or (fuzzy_if and len(matches) < threshold):
+        # fuzzy match
+        matches = [(bronte, test, quote) for bronte, result in quotes.items() for quote in result for test in tests if test in quote.lower()]
+
+    return matches
